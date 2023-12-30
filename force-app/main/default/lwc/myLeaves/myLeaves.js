9db3f33451b8f05e1358a9746857b2ce147d6953
Surajit Debnath <surajit.it11@gmail.com>
2023-12-30 17:28:43 +0530

    Leave app commit 2

---
+++ force-app/main/default/lwc/myLeaves/myLeaves.js
@@ -0,0 +1,46 @@
+import { LightningElement, wire } from 'lwc';
+import getMyLeaves from '@salesforce/apex/LeaveRequstController.getMyLeaves';
+
+const COLUMNS=[
+    { label: 'Request Id', fieldName: 'Name', cellAttributes:{class:{fieldName:'cellClass'}}},
+    { label: 'From Date', fieldName: 'From_Date__c', cellAttributes:{class:{fieldName:'cellClass'}}},
+    { label: 'To Date', fieldName: 'To_Date__c', cellAttributes:{class:{fieldName:'cellClass'}}},
+    { label: 'Reason', fieldName: 'Reason__c', cellAttributes:{class:{fieldName:'cellClass'}}},
+    { label: 'Status', fieldName: 'Status__c', cellAttributes:{class:{fieldName:'cellClass'}}},
+    { label: 'Manager Comment', fieldName: 'Manager_Comment__c', cellAttributes:{class:{fieldName:'cellClass'}}},
+    {
+        type:"button", typeAttributes:
+        {
+            label:'Edit',
+            name:'Edit',
+            title:'Edit',
+            value:'Edit',
+            disabled:{fieldName:'isEditDisabled'}
+        }, cellAttributes:{class:{fieldName:'cellClass'}},
+    }
+];
+export default class MyLeaves extends LightningElement {
+
+    myLeaves = [];
+    myLeavesWireResult;
+    columns=COLUMNS;
+    showModalPopup = false;
+
+    @wire(getMyLeaves)
+    wiredMyLeaves(result){
+        if(result.data){
+            this.myLeaves = result.data.map(a => ({
+                ...a,
+                cellClass: a.Status__c == 'Approved' ? 'slds-theme_success': a.Status__c == 'Rejected' ? 'slds-theme_warning' : '',
+                isEditDisabled: a.Status__c != 'Pending'
+        }));
+        }
+        if(result.error){
+            console.log('Error while fetching leaves --> ', result.error);
+        }
+    }
+
+    get noRecordsFound(){
+        return this.myLeaves.length == 0;
+    }
+}
